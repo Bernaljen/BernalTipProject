942fac06731678848d04af6e5c725f448b760a0d
LAB-49 <LAB-49@LAB-49-PC02>
2023-10-18 10:47:54 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BernalTipProject
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            double totalBill;
+            double liquorCharge;
+            double tipPercentage;
+            double tip;
+
+            Console.Write("Enter total bill: ");
+            totalBill = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter liquor charge: ");
+            liquorCharge = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter tip percentage; ");
+            tipPercentage = double.Parse(Console.ReadLine());
+
+            tip = (totalBill - liquorCharge) * tipPercentage;
+
+            Console.WriteLine($"tip:{tip}");
+
+
+        }
+    }
+}
